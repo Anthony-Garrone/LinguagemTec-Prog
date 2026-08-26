2464cb29e8923520ded29cd37a143600f1170c08
Anthony-Garrone <161462781+Anthony-Garrone@users.noreply.github.com>
2026-08-26 16:08:30 -0300

    Add files via upload

---
+++ Atividade 6/Ex9.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Ex9{
+    static char[] GerarComplementar(char[] DNA)
+    {
+        char[] complementar = new char[DNA.Length];
+
+        for(int i = 0; i < DNA.Length; i++)
+        {
+            if(DNA[i] == 'A')
+                complementar[i] = 'T';
+            else if(DNA[i] == 'T')
+                 complementar[i] = 'A';
+            else if(DNA[i] == 'C')
+                 complementar[i] = 'G';
+            else if(DNA[i] == 'G')
+                 complementar[i] = 'C';
+        }
+        return complementar;
+    }
+    static void Main(){
+        Console.Write("Digite a fita de DNA: ");
+        string dna = Console.ReadLine().ToUpper();
+
+        char[] DNA = dna.ToCharArray();
+
+        char[] complementar = GerarComplementar(DNA);
+
+        string fitaComplementar = new string(complementar);
+
+        Console.WriteLine("Fita complementar: "+fitaComplementar);
+    }
+}
