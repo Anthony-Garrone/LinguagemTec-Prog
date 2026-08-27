d25efc21187bb92dbcb88bd23e49ccccbe7c4cef
Anthony-Garrone <161462781+Anthony-Garrone@users.noreply.github.com>
2026-08-27 10:29:44 -0300

    Add files via upload

---
+++ AulaString/AulaString/Strings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class AulaStrings()
+{
+    static void Main()
+    {
+        char maiuscula = 'A';
+        char minuscula = 'a';
+        char digito = '0';
+
+        Console.WriteLine((int)maiuscula); // 65
+        Console.WriteLine((int)minuscula); // 97
+        Console.WriteLine((int)digito);    // 48
+
+
+    }
+}
